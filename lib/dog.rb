b5816d9b939e5075aaca8c1dd0b5b25725b10b46
Jasmine Chen <jasmine1226@gmail.com>
2019-08-05 01:47:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,6 +62,7 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
       dog = self.new(name: dog_data[1], breed: dog_data[2], id: dog_data[0])
+      puts "#{dog}"
     else
       dog = self.create(name: name, breed: breed)
     end
