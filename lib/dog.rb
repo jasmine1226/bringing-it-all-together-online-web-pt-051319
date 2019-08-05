8a0cdb5f5b0884cec2334283839e57dbd72a729d
Jasmine Chen <jasmine1226@gmail.com>
2019-08-05 01:11:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
 class Dog
   attr_accessor :id, :name, :breed
 
-
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+  end
 
 end
