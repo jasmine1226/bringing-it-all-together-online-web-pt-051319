247500cf9d903320b19334f2175c5fe69514f8ac
Jasmine Chen <jasmine1226@gmail.com>
2019-08-05 01:35:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,6 +48,8 @@ class Dog
       WHERE id = ?
       LIMIT 1
     SQL
-    DB[:conn].execute(sql, id)
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
